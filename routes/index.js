77e45d123a168337b0cb98b9940aab4b0f082fe6
최예찬 <cyc1007@midasit.com>
2018-07-28 18:23:09 +0900

    에러 핸들링 재수정

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.post('/createToken', (_, res) => {
 router.get('/checkToken', (req, res) => {
     const token = req.query.token;
     mongo.User.findById(token).exec().then(user => {
-        if (!user) res.json({valid: false, message: '존재하지 않는 토큰입니다.'});
+        if (!user) res.json({valid: false, message: '존재하지 않는 토큰입니다.'})
         else res.json({valid: true, message: '존재하는 토큰입니다.'});
     })
 });
@@ -18,20 +18,14 @@ router.get('/checkToken', (req, res) => {
 router.post('/backup', (req, res) => {
     const memos = req.body.memos;
     const token = req.body.token;
-    if (!Array.isArray(memos)) {
+    if (Array.isArray(memos)) {
         res.status(400).json({ message: '요청의 memos 필드가 잘못되었습니다.'});
     }
-    else mongo.User.findById(token)
-        .then(user => {
-            if (!user) res.status(400).json({ message: '존재하지 않는 토큰입니다.' });
-            else mongo.User
-                .findByIdAndUpdate(token, {$set: {data: JSON.stringify(memos)}})
-                .exec()
-                .then(() => res.json({ message: '성공적으로 저장되었습니다.'}))
-                .catch(err => res.status(500).json({ message: err.message }));
-        })
-
-
+    else mongo.User
+        .findByIdAndUpdate(token, {$set: {data: JSON.stringify(memos)}})
+        .exec()
+        .then(() => res.json({ message: '성공적으로 저장되었습니다.'}))
+        .catch(err => res.status(400).json({ message: '존재하지 않는 토큰입니다.' }));
 });
 
 router.get('/backup', (req, res) => {
